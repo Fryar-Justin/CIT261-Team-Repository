a8cf6d6b55aafe7e87d071c08c5a2d8772699acc
Kailey <dil11004@byui.edu>
2017-03-15 14:00:51 -0600

    Add files via upload

--- fryardomain_TEAM261/modules/header.php
+++ fryardomain_TEAM261/modules/header.php
@@ -4,10 +4,10 @@
     </div>
     <div id="mySidenav" class="sidenav">
         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <a href="#">Practice</a>
+        <a href="/modules/timer.php">Practice</a>
         <a href="#">Calendar</a>
         <p>Resources</p>
-        <a href="#">  Tuning</a>
+        <a href="/modules/tuning.php">  Tuning</a>
         <a href="#">  Rhythms</a>
         <a href="#">  Duets</a>
         <a href="#">  Charts</a>
