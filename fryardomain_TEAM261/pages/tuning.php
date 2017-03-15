2191fe80fa29446dbe7b21a7c642cb7f97934e20
Kailey <dil11004@byui.edu>
2017-03-15 14:00:25 -0600

    Add files via upload

---
+++ fryardomain_TEAM261/pages/tuning.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Music Mania</title>
+        <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/head.php'; ?>         <!--Contains the CSS and Google Fonts links-->
+        <script>
+            <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/javascript/navigation.js'; ?>                 // Contains navigation bar scripts
+            <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/javascript/***LINK JAVASCRIPT FILE***.js'; ?> // Contains Scripts for this page
+        </script>
+    </head>
+    <body>
+        <header>
+            <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/header.php'; ?>                               <!--Contains navigation tags-->
+        </header>
+        <main>
+            <div class="rightContent">
+                <h2>A</h2>
+        <audio controls>
+		<source src="/media/drones/A.mp3" type="audio/mpeg">
+		The browser you are using does not support this audio element.
+		</audio>
+        <h2>Ab</h2>
+        <audio controls>
+		<source src="/media/drones/Ab.mp3" type="audio/mpeg">
+		The browser you are using does not support this audio element.
+		</audio>
+        <h2>B</h2>
+        <audio controls>
+		<source src="/media/drones/B.mp3" type="audio/mpeg">
+		The browser you are using does not support this audio element.
+		</audio>
+        <h2>C</h2>
+        <audio controls>
+		<source src="/media/drones/C.mp3" type="audio/mpeg">
+		The browser you are using does not support this audio element.
+		</audio>
+        <h2>D</h2>
+        <audio controls>
+		<source src="/media/drones/D.mp3" type="audio/mpeg">
+		The browser you are using does not support this audio element.
+		</audio>
+        <h2>Db</h2>
+        <audio controls>
+		<source src="/media/drones/Db.mp3" type="audio/mpeg">
+		The browser you are using does not support this audio element.
+		</audio>
+        <h2>E</h2>
+        <audio controls>
+		<source src="/media/drones/E.mp3" type="audio/mpeg">
+		The browser you are using does not support this audio element.
+		</audio>
+        <h2>Eb</h2>
+        <audio controls>
+		<source src="/media/drones/Eb.mp3" type="audio/mpeg">
+		The browser you are using does not support this audio element.
+		</audio>
+        <h2>F</h2>
+        <audio controls>
+		<source src="/media/drones/F.mp3" type="audio/mpeg">
+		The browser you are using does not support this audio element.
+		</audio>
+        <h2>G</h2>
+        <audio controls>
+		<source src="/media/drones/G.mp3" type="audio/mpeg">
+		The browser you are using does not support this audio element.
+		</audio>
+        <h2>Gb</h2>
+        <audio controls>
+		<source src="/media/drones/Gb.mp3" type="audio/mpeg">
+		The browser you are using does not support this audio element.
+		</audio>
+            </div>
+        </main>
+        <footer>
+            <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/footer.php'; ?>                               <!--Contains footer tags-->
+        </footer>
+    </body>
+</html>
