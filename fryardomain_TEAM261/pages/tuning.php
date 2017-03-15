1097343afb957b94f8591dd74ab8c719d5a2ca95
Kailey <dil11004@byui.edu>
2017-03-15 16:17:23 -0600

    3/15/2017 - Finished Tuning page

--- fryardomain_TEAM261/pages/tuning.php
+++ fryardomain_TEAM261/pages/tuning.php
@@ -2,11 +2,11 @@
 
 <html>
     <head>
-        <title>Music Mania</title>
+        <title>Tuning</title>
         <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/head.php'; ?>         <!--Contains the CSS and Google Fonts links-->
         <script>
             <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/javascript/navigation.js'; ?>                 // Contains navigation bar scripts
-            <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/javascript/***LINK JAVASCRIPT FILE***.js'; ?> // Contains Scripts for this page
+            <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/javascript/tuning.js'; ?> // Contains Scripts for this page
         </script>
     </head>
     <body>
@@ -15,61 +15,26 @@
         </header>
         <main>
             <div class="rightContent">
-                <h2>A</h2>
-        <audio controls>
-		<source src="/media/drones/A.mp3" type="audio/mpeg">
-		The browser you are using does not support this audio element.
-		</audio>
-        <h2>Ab</h2>
-        <audio controls>
-		<source src="/media/drones/Ab.mp3" type="audio/mpeg">
-		The browser you are using does not support this audio element.
-		</audio>
-        <h2>B</h2>
-        <audio controls>
-		<source src="/media/drones/B.mp3" type="audio/mpeg">
-		The browser you are using does not support this audio element.
-		</audio>
-        <h2>C</h2>
-        <audio controls>
-		<source src="/media/drones/C.mp3" type="audio/mpeg">
-		The browser you are using does not support this audio element.
-		</audio>
-        <h2>D</h2>
-        <audio controls>
-		<source src="/media/drones/D.mp3" type="audio/mpeg">
-		The browser you are using does not support this audio element.
-		</audio>
-        <h2>Db</h2>
-        <audio controls>
-		<source src="/media/drones/Db.mp3" type="audio/mpeg">
-		The browser you are using does not support this audio element.
-		</audio>
-        <h2>E</h2>
-        <audio controls>
-		<source src="/media/drones/E.mp3" type="audio/mpeg">
-		The browser you are using does not support this audio element.
-		</audio>
-        <h2>Eb</h2>
-        <audio controls>
-		<source src="/media/drones/Eb.mp3" type="audio/mpeg">
-		The browser you are using does not support this audio element.
-		</audio>
-        <h2>F</h2>
-        <audio controls>
-		<source src="/media/drones/F.mp3" type="audio/mpeg">
-		The browser you are using does not support this audio element.
-		</audio>
-        <h2>G</h2>
-        <audio controls>
-		<source src="/media/drones/G.mp3" type="audio/mpeg">
-		The browser you are using does not support this audio element.
-		</audio>
-        <h2>Gb</h2>
-        <audio controls>
-		<source src="/media/drones/Gb.mp3" type="audio/mpeg">
-		The browser you are using does not support this audio element.
-		</audio>
+                
+                <div id="noteSelect">
+                    <select onchange="noteChoice(value)">
+                        <option value="Select">Select a Note</option>
+                     <option value="A">A</option>
+                        <option value="A♭">A&#9837;</option>
+                        <option value="B">B</option>
+                        <option value="B♭">B&#9837;</option>
+                        <option value="C">C</option>
+                        <option value="D">D</option>
+                        <option value="D♭">D&#9837;</option>
+                        <option value="E">E</option>
+                        <option value="E♭">E&#9837;</option>
+                        <option value="F">F</option>
+                        <option value="G">G</option>
+                        <option value="G♭">G&#9837;</option>
+                    </select>
+                </div>
+                <div id="noteView"></div>
+
             </div>
         </main>
         <footer>
