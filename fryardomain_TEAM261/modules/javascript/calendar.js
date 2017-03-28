d6a5bc0b348d40fa9332452295de8301fd5a01ab
justinfryar <justinfryar@10.0.1.7>
2017-03-27 22:33:28 -0600

    Added some decoration to the calendar page

--- fryardomain_TEAM261/modules/javascript/calendar.js
+++ fryardomain_TEAM261/modules/javascript/calendar.js
@@ -1,8 +1,20 @@
-function draw(canvas, image){
-    var c = document.getElementById(canvas);
+function intervalTimer(canvasId, imageId){
+    var interval = setInterval(draw, 3000, canvasId, imageId);
+}
+
+function draw(canvasId, imageId){
+    var c = document.getElementById(canvasId);
     var ctx = c.getContext("2d");
-    var img = document.getElementById(image);
+    var img = document.getElementById(imageId);
+    var randomX = Math.floor(Math.random() * 123);
+    var randomY = Math.floor(Math.random() * 567);
     
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(img , 0, 0, 140, 140);
+//    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.drawImage(img , randomX, randomY);
+    document.getElementById("output").innerHTML = "X-Random: " + randomX + "<br>" +
+                                                  "Y-Random: " + randomY + "<br>" +
+                                                  "Canvas Width: " + c.width + "<br>" + 
+                                                  "Canvas Height: " + c.height + "<br>" + 
+                                                  "Image Width: " + img.width + "<br>" + 
+                                                  "Image Height: " + img.height;
 }
